d8715963644c80c74910cc955c0aa20654792068
Simon Krüger <simon.krueger@intive.com>
2024-07-18 12:58:59 +0200

    current geo position

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,114 @@
 'use client';
 import Image from 'next/image';
-import Link from 'next/link';
 import React, { useEffect, useRef } from "react";
 
 export default function Home() {
-  const objectLoaded = () => {
-    const objElement = objectRef.current;
-
-    if (objElement) {
-      console.log("objElement", objElement);
-      const svgDocument = objElement.getSVGDocument();
-      const countries = ["DE", "AT", "IT", "ES", "FR", "PT", "IT-SA", "HR", "FI", "EN", "EG", "TH", "US", "LU", "BE", "NL", "PL", "FR-GP", "MT", "MT-GZ", "GR"];
-      countries.forEach((country) => {
-        svgDocument?.getElementById(country)?.setAttribute("fill", "gray");
-      });
-    }
-  };
-
   const objectRef = useRef<HTMLObjectElement>(null);
 
   useEffect(
     () => {
       const objElement = objectRef.current;
-      objectLoaded();
+      mapLoaded();
       if (objElement) {
-        objElement.addEventListener("load", objectLoaded);
+        objElement.addEventListener("load", mapLoaded);
         return () => {
-          objElement.removeEventListener("load", objectLoaded);
+          objElement.removeEventListener("load", mapLoaded);
         };
       }
     }, 
     []
   );
+  
+  interface Point {
+    x: number;
+    y: number;
+  }
+
+  function polygons(path: SVGPathElement): SVGPointList {
+    var NUM_POINTS = 6;
+    var len = path.getTotalLength();
+    var points = new SVGPointList();
+
+    for (var i=0; i < NUM_POINTS; i++) {
+        var pt = path.getPointAtLength(i * len / (NUM_POINTS-1));
+        points.appendItem(new SVGPoint(pt.x, pt.y));
+    }
+
+    return points;
+  }
+
+  function area(pts: SVGPointList): number {
+    var area = 0;
+    var nPts = pts.length;
+    var j = nPts - 1;
+    var p1: Point;
+    var p2: Point;
+
+    for (var i = 0; i < nPts; j = i++) {
+      p1 = pts[i];
+      p2 = pts[j];
+      area += p1.x * p2.y;
+      area -= p1.y * p2.x;
+    }
+    area /= 2;
+    return area;
+  };
+
+  function computeCentroid(pts: SVGPointList) {
+      var nPts = pts.length;
+      var x=0; var y=0;
+      var f;
+      var j=nPts-1;
+      var p1; var p2;
+
+      for (var i=0;i<nPts;j=i++) {
+          p1=pts[i]; p2=pts[j];
+          f=p1.x*p2.y-p2.x*p1.y;
+          x+=(p1.x+p2.x)*f;
+          y+=(p1.y+p2.y)*f;
+      }
+
+      f=area(pts)*6;
+      return [x/f,y/f];
+  };
+  
+  const mapLoaded = () => {
+    const svgDocument = objectRef.current?.getSVGDocument();
+    const countries = ["DE", "AT", "IT", "ES", "FR", "PT", "IT-SA", "HR", "FI", "EN", "EG", "TH", "US", "LU", "BE", "NL", "PL", "FR-GP", "MT", "MT-GZ", "GR"];
+    countries.forEach((country) => {
+      svgDocument?.getElementById(country)?.setAttribute("fill", "gray");
+    });
+    svgDocument?.getElementById("GEO")?.setAttribute("fill", "red");
+    svgDocument?.getElementById("GEO")?.setAttribute("stroke", "white");
+    svgDocument?.getElementById("GEO")?.setAttribute("stroke-width", "1");
+    
+    const country = svgDocument?.getElementById("DE") as SVGPolygonElement | null;
+    if (country) {
+      // const center = computeCentroid(polygons(country));
+      // console.log(center);
+      // svgDocument?.getElementById("GEO")?.setAttribute("transform", "translate(center.x, center.y)");
+      svgDocument?.getElementById("GEO")?.setAttribute("transform", "translate(1043.9,185.9)");
+    }
+    
+
+    // All paths for hover effect
+    // objectRef.current?.contentDocument?.querySelectorAll("path").forEach((path) => {
+    //   path.addEventListener("mouseenter", () => {
+    //     if (countries.includes(path.id)) {
+    //       path.setAttribute("fill", "gray");
+    //     } else {
+    //       path.setAttribute("fill", "blue");
+    //     }
+    //   });
+    //   path.addEventListener("mouseleave", () => {
+    //     if (countries.includes(path.id)) {
+    //       path.setAttribute("fill", "gray");
+    //     } else {
+    //       path.setAttribute("fill", "#ececec");
+    //     }
+    //   });
+    // });
+  };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -51,7 +128,7 @@ export default function Home() {
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
         <object 
           ref={objectRef}
-          onLoad={objectLoaded}
+          onLoad={mapLoaded}
           className="map"
           type="image/svg+xml"
           data="world.svg"
