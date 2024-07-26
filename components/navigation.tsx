2ad3a472fea04c2866915024856794f6b616a04f
Simon Krüger <simon.krueger@intive.com>
2024-07-26 17:51:04 +0200

    mobile menu correction

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,98 +1,13 @@
 'use client';
-import { Fragment, useState } from 'react';
+import { useState } from 'react';
 import { Dialog, Disclosure, Popover, Transition } from '@headlessui/react';
 import {
-  ArrowPathIcon,
   Bars3Icon,
-  ChartPieIcon,
-  CursorArrowRaysIcon,
-  FingerPrintIcon,
-  SquaresPlusIcon,
   XMarkIcon,
 } from '@heroicons/react/24/outline';
 import {
   ChevronDownIcon,
-  PhoneIcon,
-  PlayCircleIcon,
 } from '@heroicons/react/20/solid';
-
-const blogCategories = [
-  {
-    name: 'Europe',
-    description: 'Shaping the future of the In-Car Experience',
-    href: '#',
-    icon: ChartPieIcon,
-  },
-  {
-    name: 'North America',
-    description: 'Disruptive fintech solutions are built around users',
-    href: '#',
-    icon: CursorArrowRaysIcon,
-  },
-  {
-    name: 'South America',
-    description: 'High-bandwidth and low-latency connectivity',
-    href: '#',
-    icon: FingerPrintIcon,
-  },
-  {
-    name: 'Africa',
-    description: 'Powering the customer journey',
-    href: '#',
-    icon: SquaresPlusIcon,
-  },
-  {
-    name: 'Asia',
-    description: 'Stand up to digital disruption',
-    href: '#',
-    icon: ArrowPathIcon,
-  },
-  {
-    name: 'Oceania',
-    description: 'Enable your competitive edge',
-    href: '#',
-    icon: ArrowPathIcon,
-  },
-];
-const blogCallToAction = [
-  { name: 'Read all', href: 'https://cr0ss.org/', icon: PlayCircleIcon },
-  // { name: 'Contact sales', href: '#', icon: PhoneIcon },
-];
-
-const services = [
-  {
-    name: 'Digital Advisory',
-    description: 'Explore all your possibilities',
-    href: '#',
-    icon: ChartPieIcon,
-  },
-  {
-    name: 'Managed Services',
-    description: 'Free up your resources, leverage our expertise',
-    href: '#',
-    icon: CursorArrowRaysIcon,
-  },
-  {
-    name: 'Design',
-    description: 'Create people-centric digital products',
-    href: '#',
-    icon: FingerPrintIcon,
-  },
-  {
-    name: 'Engineering',
-    description: 'Build amazing digital products',
-    href: '#',
-    icon: ArrowPathIcon,
-  },
-];
-const servicesCallsToAction = [
-  { name: 'Read more', href: '#', icon: PlayCircleIcon },
-  { name: 'Contact sales', href: '#', icon: PhoneIcon },
-];
-
-function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(' ');
-}
 
 export default function Navigation() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -121,13 +36,13 @@ export default function Navigation() {
         </div>
 
         <Popover.Group className='hidden lg:flex lg:gap-x-12'>
-          <a href='https://cr0ss.org/blog/' className='text-sm font-semibold leading-6 text-gray-900'>
+          <a href='https://cr0ss.org/blog' className='text-sm font-semibold leading-6 text-gray-900'>
             Blog
           </a>
-          <a href='https://cr0ss.org/about/' className='text-sm font-semibold leading-6 text-gray-900'>
+          <a href='https://cr0ss.org/about' className='text-sm font-semibold leading-6 text-gray-900'>
             About
           </a>
-          <a href='https://cr0ss.org/imprint/' className='text-sm font-semibold leading-6 text-gray-900'>
+          <a href='https://cr0ss.org/imprint' className='text-sm font-semibold leading-6 text-gray-900'>
             Imprint
           </a>
         </Popover.Group>
@@ -162,96 +77,34 @@ export default function Navigation() {
           <div className='mt-6 flow-root'>
             <div className='-my-6 divide-y divide-gray-500/10'>
               <div className='space-y-2 py-6'>                
-                <Disclosure as='div' className='-mx-3'>
-                  {({ open }) => (
-                    <>
-                      <Disclosure.Button className='flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50'>
-                        Blog
-                        <ChevronDownIcon
-                          className={classNames(
-                            open ? 'rotate-180' : '',
-                            'h-5 w-5 flex-none'
-                          )}
-                          aria-hidden='true'
-                        />
-                      </Disclosure.Button>
-                      <Disclosure.Panel className='mt-2 space-y-2'>
-                        {[...blogCategories, ...blogCallToAction].map(
-                          (item) => (
-                            <Disclosure.Button
-                              key={item.name}
-                              as='a'
-                              href={item.href}
-                              className='block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50'
-                            >
-                              {item.name}
-                            </Disclosure.Button>
-                          )
-                        )}
-                      </Disclosure.Panel>
-                    </>
-                  )}
-                </Disclosure>
-                <Disclosure as='div' className='-mx-3'>
-                  {({ open }) => (
-                    <>
-                      <Disclosure.Button className='flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50'>
-                        Services
-                        <ChevronDownIcon
-                          className={classNames(
-                            open ? 'rotate-180' : '',
-                            'h-5 w-5 flex-none'
-                          )}
-                          aria-hidden='true'
-                        />
-                      </Disclosure.Button>
-                      <Disclosure.Panel className='mt-2 space-y-2'>
-                        {[...services, ...servicesCallsToAction].map((item) => (
-                          <Disclosure.Button
-                            key={item.name}
-                            as='a'
-                            href={item.href}
-                            className='block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50'
-                          >
-                            {item.name}
-                          </Disclosure.Button>
-                        ))}
-                      </Disclosure.Panel>
-                    </>
-                  )}
-                </Disclosure>
+                
                 <a
-                  href='#'
+                  href='https://cr0ss.org/blog'
                   className='-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50'
                 >
-                  Insights
+                  Blog
                 </a>
                 <a
-                  href='#'
+                  href='https://cr0ss.org/about'
                   className='-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50'
                 >
                   About
                 </a>
                 <a
-                  href='#'
+                  href='https://cr0ss.org/imprint'
                   className='-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50'
                 >
-                  Careers
+                  Imprint
                 </a>
-                <a
-                  href='#'
-                  className='-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50'
-                >
-                  Contact
-                </a>
+               
               </div>
               <div className='py-6'>
-                <a
+                {/* <a
                   href='#'
                   className='-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50'
                 >
                   Log in
-                </a>
+                </a> */}
               </div>
             </div>
           </div>
