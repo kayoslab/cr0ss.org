7d8515db8277c38d15daada024083d72715c6530
Simon Krüger <simon.krueger@intive.com>
2024-08-01 10:56:50 +0200

    add KV and update location api route

--- app/api/location/route.ts
+++ app/api/location/route.ts
@@ -1,15 +1,18 @@
 import { NextResponse } from 'next/server';
-import { revalidatePath, revalidateTag } from 'next/cache';
+import { kv } from "@vercel/kv";
 import { env } from '@/env';
 
 export async function POST(request: Request) {
   const requestHeaders = new Headers(request.headers);
   const secret = requestHeaders.get('token');
+  const locationKey = 'GEOLOCATION';
+  const locationThreshold = 150;
 
   if (secret !== env.LOCATION_API_SECRET) {
     return NextResponse.json({ message: 'Invalid secret' }, { status: 401 });
   }
 
+  const storedLocation = await kv.get<{ lat: number; lon: number }[]>(locationKey);
   const body = await request.json();
 
   const lat = body.lat;
@@ -19,9 +22,41 @@ export async function POST(request: Request) {
     return NextResponse.json({ message: 'No geo position provided' }, { status: 400 });
   }
 
-  const location = { lat, lon };
+  const currentLocation = { lat, lon };
+
+  if (!storedLocation) {
+    await kv.set(locationKey, currentLocation);
+  } else {
+    const storedLat = storedLocation[0].lat;
+    const storedLon = storedLocation[0].lon;
+
+    const distance = distanceInKmBetweenEarthCoordinates(storedLat, storedLon, lat, lon);
+
+    if (distance > locationThreshold) {
+      await kv.set(locationKey, currentLocation);
+    }
+  }
 
   // Revalidate the stored geo location if difference is significant
 
   return NextResponse.json({ revalidated: true, now: Date.now() });
+}
+
+function degreesToRadians(degrees: number) {
+  return degrees * Math.PI / 180;
+}
+
+function distanceInKmBetweenEarthCoordinates(lat1: number, lon1: number, lat2: number, lon2: number) {
+  var earthRadiusKm = 6371;
+
+  var dLat = degreesToRadians(lat2-lat1);
+  var dLon = degreesToRadians(lon2-lon1);
+
+  lat1 = degreesToRadians(lat1);
+  lat2 = degreesToRadians(lat2);
+
+  var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+          Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  return earthRadiusKm * c;
 }
