245da6611f53b54a6d6e48ab3596e36b6391e4e6
Simon Krüger <hello@cr0ss.org>
2024-10-19 20:39:49 +0200

    remove static all pages generation

--- lib/contentful/api/page.ts
+++ lib/contentful/api/page.ts
@@ -7,20 +7,6 @@ function extractPageEntries(fetchResponse: {
   return fetchResponse?.data?.pagesCollection?.items;
 }
 
-export async function getAllPages(limit = 10) {
-  const pages = await fetchGraphQL(
-    `query {
-      pageCollection(where:{slug_exists: true}, order: date_DESC, limit: ${limit}, preview: false) {
-          items {
-            ${PAGE_GRAPHQL_FIELDS}
-          }
-        }
-      }`,
-    ['pages']
-  );
-  return pages.data.pageCollection.items;
-}
-
 export async function getPage(slug: string) {
   const pages = await fetchGraphQL(
     `query {
