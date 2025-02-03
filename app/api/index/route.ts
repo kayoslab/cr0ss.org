34773070e4b7ed1f8cebf785822117d2a6ebf155
Simon Krüger <hello@cr0ss.org>
2025-02-03 14:51:58 +0100

    generate xml file on the fly

--- app/api/index/route.ts
+++ app/api/index/route.ts
@@ -5,8 +5,6 @@ import { BlogProps } from '@/lib/contentful/api/props/blog';
 import { CategoryProps } from '@/lib/contentful/api/props/category';
 // import { documentToPlainTextString } from "@contentful/rich-text-plain-text-renderer";
 import { algoliasearch } from "algoliasearch";
-import fs from "fs";
-var RSS = require('rss');
 
 function getErrorMessage(error: unknown) {
 	if (error instanceof Error) return error.message
@@ -21,10 +19,16 @@ export async function POST(request: Request) {
         // Run operations in parallel
         await Promise.all([
             updateAlgoliaIndex(request),
-            updateRSSFeed(request)
         ]);
 
-        return NextResponse.json({ revalidated: true, now: Date.now() }, { status: 200 });
+        return NextResponse.json(
+            { 
+                revalidated: true, 
+                now: Date.now() 
+            }, { 
+                status: 200
+            }
+        );
     } catch (error) {
         return NextResponse.json({ 
             message: 'Error processing request: ' + getErrorMessage(error) 
@@ -77,37 +81,4 @@ async function updateAlgoliaIndex(request: Request) {
     } catch(err) {
         throw new Error('Could not update index: ' + getErrorMessage(err));
     }
-}
-
-async function updateRSSFeed(request: Request) {
-    const allPosts = await getAllBlogs();
-    const site_url = "https://cr0ss.org";
-
-    const feedOptions = {
-        title: "cr0ss.org",
-        description: "Personal and professional website on cr0ss.org!",
-        site_url: site_url,
-        feed_url: `${site_url}/rss.xml`,
-        // image_url: `${site_url}/logo.jpeg`,
-        pubDate: new Date(),
-        copyright: `All rights reserved ${new Date().getFullYear()}`,
-    };
-
-    const feed = new RSS(feedOptions);
-
-    allPosts.map((post: BlogProps) => {
-        feed.item({
-            title: post.title,
-            description: post.summary,
-            url: `${site_url}/blog/${post.slug}`,
-            date: post.sys.createdAt,
-        });
-    });
-
-    // Write to the public directory instead of a relative path
-    const publicDir = process.cwd() + '/public';
-    if (!fs.existsSync(publicDir)){
-        fs.mkdirSync(publicDir, { recursive: true });
-    }
-    fs.writeFileSync(`${publicDir}/rss.xml`, feed.xml({ indent: true }));
 }
