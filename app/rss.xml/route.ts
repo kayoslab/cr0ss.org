af4213564ce973e7a9f39bc83d38b2c3a74e0093
Simon Krüger <hello@cr0ss.org>
2025-02-03 14:51:58 +0100

    generate xml file on the fly

---
+++ app/rss.xml/route.ts
@@ -0,0 +1,36 @@
+import { NextResponse } from 'next/server';
+import { getAllBlogs } from '@/lib/contentful/api/blog';
+import { BlogProps } from '@/lib/contentful/api/props/blog';
+var RSS = require('rss');
+
+export async function GET() {
+    const allPosts = await getAllBlogs();
+    const site_url = "https://cr0ss.org";
+
+    const feedOptions = {
+        title: "cr0ss.org",
+        description: "Personal and professional website on cr0ss.org!",
+        site_url: site_url,
+        feed_url: `${site_url}/rss.xml`,
+        pubDate: new Date(),
+        copyright: `All rights reserved ${new Date().getFullYear()}`,
+    };
+
+    const feed = new RSS(feedOptions);
+
+    allPosts.map((post: BlogProps) => {
+        feed.item({
+            title: post.title,
+            description: post.summary,
+            url: `${site_url}/blog/${post.slug}`,
+            date: post.sys.createdAt,
+        });
+    });
+
+    return new NextResponse(feed.xml({ indent: true }), {
+        headers: {
+            'Content-Type': 'application/xml',
+            'Cache-Control': 's-maxage=3600, stale-while-revalidate',
+        },
+    });
+} 
