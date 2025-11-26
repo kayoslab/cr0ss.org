ee8333ad88ba35724515ca5b0b1227b4fb492c7e
Simon Krüger <hello@cr0ss.org>
2025-11-26 10:22:50 +0100

    Show loading of dashboard immediately to prevent feeling of slow response

---
+++ app/dashboard/loading.tsx
@@ -0,0 +1,11 @@
+import DashboardSkeleton from "./Dashboard.skeleton";
+
+export default function Loading() {
+  return (
+    <main className="min-h-screen overflow-x-hidden bg-white">
+      <section className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-10">
+        <DashboardSkeleton />
+      </section>
+    </main>
+  );
+}
