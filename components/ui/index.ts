70c8648753e03a427fd1f8a7b1cd4aee50c479d1
Simon Krüger <hello@cr0ss.org>
2025-12-01 14:07:25 +0100

    Remove requirement for tremor charts and migrate to shadcn

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -3,17 +3,13 @@
  * Reusable, accessible components following WCAG 2.1 AA standards
  */
 
-export { Button, type ButtonProps } from './button';
-export { Badge, type BadgeProps } from './badge';
-export { Input, type InputProps } from './input';
+export { Button } from './button';
+export { Badge } from './badge';
+export { Input } from './input';
 export {
   Card,
   CardHeader,
   CardBody,
   CardFooter,
-  type CardProps,
-  type CardHeaderProps,
-  type CardBodyProps,
-  type CardFooterProps,
 } from './card';
-export { Skeleton, SkeletonCard } from './skeleton';
+export { Skeleton } from './skeleton';
